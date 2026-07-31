05a10518500495b9e7b56c5b8a2a5e89d85764e3
youki258 <youki258@users.noreply.github.com>
2026-07-31 20:41:33 +0800

    feat: redesign homepage and make blog single-page

--- src/config.ts
+++ src/config.ts
@@ -40,7 +40,12 @@ export const siteConfig: SiteConfig = {
 };
 
 export const navBarConfig: NavBarConfig = {
-	links: [LinkPreset.Home, LinkPreset.Archive, LinkPreset.About],
+	links: [
+		LinkPreset.Home,
+		{ name: "博客", url: "/blog/" },
+		LinkPreset.Archive,
+		LinkPreset.About,
+	],
 };
 
 export const profileConfig: ProfileConfig = {
--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,5 +1,3 @@
-export const PAGE_SIZE = 8;
-
 export const LIGHT_MODE = "light",
 	DARK_MODE = "dark",
 	AUTO_MODE = "auto";
